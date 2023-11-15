a825ce174a9ebaf4897aa41fe5985290fbdd61bf
halitcinici <halit.cinici@ebp.com>
2023-11-15 10:56:11 +0100

    interception d'un evenement

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -10,11 +10,28 @@ export class FaceSnapComponent implements OnInit {
   description!: string;
   createdDate!: Date;
   snaps!: number;
+  imageUrl!: string;
+  clickSnap!: string;
 
   ngOnInit(){
     this.title = "Mon titre";
     this.description = "Ma description";
     this.createdDate = new Date();
     this.snaps = 6;
+    this.imageUrl = 'https://th.bing.com/th/id/OIG.0j67NMJ_6.el9IX1b.XM?pid=ImgGn&w=1024&h=1024&rs=1';
+    this.clickSnap = "Oh snap!"
   }
+
+  onClickSnap(){
+   
+    if (this.clickSnap == "Oups, déjà cliqué"){
+      this.snaps--;
+      this.clickSnap = "Oh snap!"
+    } else {
+      this.snaps++;
+      this.clickSnap = "Oups, déjà cliqué"
+    }
+  }
+
+
 }
