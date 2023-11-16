3c5c9e35bdda079ccbd494cf47de994b7a53d1ba
halitcinici <halit.cinici@ebp.com>
2023-11-16 08:34:23 +0100

    mise en place des directives et styles dynamiques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,37 @@ import { FaceSnap } from './models/face-snap.models';
 })
 export class AppComponent implements OnInit {
 
-  mysnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps! : FaceSnap[];
+
+
 
   ngOnInit(){
-    this.mysnap = new FaceSnap(
-      'Un fruit de la passion qui galope',
-      'Un événement étrange pour un fruit',
-      new Date(),
-      0,
-      'https://th.bing.com/th/id/OIG.0j67NMJ_6.el9IX1b.XM?pid=ImgGn&w=1024&h=1024&rs=1'
-    )
-
-    this.myOtherSnap = new FaceSnap(
-      'Une banane etrange',
-      'Une banane qui fume',
-      new Date(),
-      0,
-      'https://th.bing.com/th/id/OIG.eaqEgfDGoF3tq4GRb10S?w=270&h=270&c=6&r=0&o=5&pid=ImgGn'
-    )
-
-    this.myLastSnap = new FaceSnap(
-      'La pasthèque championne',
-      'La pasthèque comme vous ne lavez jamais vu',
-      new Date(),
-      0,
-      'https://th.bing.com/th/id/OIG.N7pSir8ggZSH8Rd1Zo0g?w=270&h=270&c=6&r=0&o=5&pid=ImgGn'
-    )
+    this.faceSnaps = [
+      {
+        title : 'Un fruit de la passion qui galope',
+        description : 'Un événement étrange pour un fruit',
+        createdDate : new Date(),
+        snaps : 80,
+        imageUrl : 'https://th.bing.com/th/id/OIG.0j67NMJ_6.el9IX1b.XM?pid=ImgGn&w=1024&h=1024&rs=1',
+        location: 'Paris'
+      },
+      {
+        title: 'Une banane etrange',
+        description: 'Une banane qui fume',
+        createdDate: new Date(),
+        snaps: 160,
+        imageUrl: 'https://th.bing.com/th/id/OIG.eaqEgfDGoF3tq4GRb10S?w=270&h=270&c=6&r=0&o=5&pid=ImgGn',
+        location: 'Marseille'
+      },
+      {
+        title: 'La pasthèque championne',
+        description: 'La pasthèque comme vous ne lavez jamais vu',
+        createdDate: new Date(),
+        snaps: 240,
+        imageUrl: 'https://th.bing.com/th/id/OIG.N7pSir8ggZSH8Rd1Zo0g?w=270&h=270&c=6&r=0&o=5&pid=ImgGn'
+      }
+    ]
+      
+    
   }
 }
