e9c13e597b5764ed425a0f2948013592d8e015ee
Halit <halit.cinici@live.fr>
2023-11-20 19:24:20 +0100

    implementation des formulaires de templates

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,15 +9,21 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponent implements OnInit {
 
+  userEmail!: string;
+
   constructor(private router: Router){
 
   }
 
   ngOnInit(): void {
-    
+
   }
 
   onContinue(): void {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm): void{
+    console.log(form.value);
   }
 }
