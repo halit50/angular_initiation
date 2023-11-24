c856964c7b8823fe40225c9d2f2a40adc56d63e9
halitcinici <halit.cinici@ebp.com>
2023-11-24 17:58:48 +0100

    refactor et mise en place des modules

--- src/app/face-snaps/components/add-face-snap/add-face-snap.component.ts
+++ src/app/face-snaps/components/add-face-snap/add-face-snap.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { FaceSnap } from '../models/face-snap.models';
+import { FaceSnap } from '../../../core/models/face-snap.models';
 import { Observable, map, tap } from 'rxjs';
-import { FaceSnapsService } from '../services/face-snaps.service';
+import { FaceSnapsService } from '../../../core/services/face-snaps.service';
 import { Router } from '@angular/router';
 
 @Component({
